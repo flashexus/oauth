388eeddf3f84141df3bdaa57908295204e75cfc9
iwata_k@vital <iwata_k@vitallead.co.jp>
2022-04-12 19:19:43 +0900

    アクセストークンを発行できるところまで確認

--- lib/omniauth/strategies/doorkeeper.rb
+++ lib/omniauth/strategies/doorkeeper.rb
@@ -5,11 +5,11 @@ module OmniAuth
       include OmniAuth::Strategy
       option :name, :doorkeeper # strategyの名前　ここで指定した名前をdeviseで呼び出す
       option :client_options, {
-        site: 'http://localhost:3001',
+        site: 'http://keyrock:3001',
         authorize_url: 'http://localhost:3001/oauth2/authorize',
-        ssl: { verify: false }
+        token_url: 'http://keyrock:3001/oauth2/token',
+        ssl: { verify: false } 
       }
-      #option :client_options, { ssl: { verify: false } }
 
       # uidとして設定するデータを指定
       uid { raw_info['user']['id'] }
@@ -20,12 +20,16 @@ module OmniAuth
 
       # providerのAPIを叩いて、データを取ってくる
       def raw_info
-        @raw_info ||= access_token.get('/oauth2/token').parsed
+        p '------cool raw_info --------'
+        p access_token
+        @raw_info ||= access_token.get('/user').parsed
+
+        p @raw_info
       end
 
       # リクエストにカスタムパラメータを追加
       def request_phase
-        p '------cool request_phase--------'
+        p '------cool request_phase --------'
         p callback_url
         _authorize_params = { :redirect_uri => callback_url }.merge(authorize_params)
      #   _authorize_params.merge!({ group_id: request.env['omniauth.params']['group_id'] })
